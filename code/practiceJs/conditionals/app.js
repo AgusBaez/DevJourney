ba7ac7d0f52cec423cd3104f4c480368e1818d1e
AgusBaez <agustinbaezignacio@gmail.com>
2022-06-13 23:38:48 -0300

    ifBoolRamdon

---
+++ code/practiceJs/conditionals/app.js
@@ -0,0 +1,15 @@
+console.log(`My conditional`);
+let ramd = Math.round(Math.random() * (10 - 0))
+console.log(`This is the number "${ramd}" for my conditionl`);
+if (ramd > 0.5) {
+    console.log(`The num ramdon is greater than 0.5`);
+} else {
+    console.log(`The num ramdon is less than 0.5 :(`);
+}
+console.log(`Other Conditional`);
+
+console.log(`This is the number "${ramd}" for my conditionl`);
+if(ramd %  2 === 0) {
+    console.log("even")
+}
+console.log(`The end`);
