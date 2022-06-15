d4505646ed53d5bd8afc8d91377beb55e9003175
AgusBaez <agustinbaezignacio@gmail.com>
2022-06-15 15:27:09 -0300

    arrayMethods

--- code/practiceJs/array/app.js
+++ code/practiceJs/array/app.js
@@ -1,6 +1,17 @@
-const leaderboard = ["Harry", "Lua", "Hermione", "Bellatrix"]; //DON'T TOUCH THIS LINE!
-console.log(leaderboard);
-// YOUR CODE BELOW HERE:
+//Corregion de string en el array
+const leaderboard = ["Harry", "Lua", "Hermione", "Bellatrix"]; //Corregir columna 1 fila 2 y 4;
+console.log(`Original array - [${leaderboard}]`);
 leaderboard[1] = "Luna";
 leaderboard[3] = "Draco";
-leaderboard.length;
+console.log(`Chaged array - [${leaderboard}]`);
+
+//Agregar y Sacar item en un arrat
+const planets = ["The Moon", "Venus", "Earth", "Mars", "Jupiter"]; //DO NOT TOUCH THIS LINE!
+console.log(`Bad Array about planets - [${planets}]`);
+planets.shift();
+planets.push("Saturn");
+planets.unshift("Mercury");
+console.log(`New Array Planets - [${planets}]`);
+
+//Concatenacion
+let allArray = planets.concat(leaderboard); console.log(allArray);
