b278745557b514698f5d2e45dad44af5ea0a941c
AgusBaez <agustinbaezignacio@gmail.com>
2022-06-15 17:07:35 -0300

    moreMethodsArray

--- code/practiceJs/array/app.js
+++ code/practiceJs/array/app.js
@@ -5,8 +5,8 @@ leaderboard[1] = "Luna";
 leaderboard[3] = "Draco";
 console.log(`Chaged array - [${leaderboard}]`);
 
-//Agregar y Sacar item en un arrat
-const planets = ["The Moon", "Venus", "Earth", "Mars", "Jupiter"]; //DO NOT TOUCH THIS LINE!
+//Agregar y Sacar item en un array
+const planets = ["The Moon", "Venus", "Earth", "Mars", "Jupiter"];
 console.log(`Bad Array about planets - [${planets}]`);
 planets.shift();
 planets.push("Saturn");
@@ -14,4 +14,20 @@ planets.unshift("Mercury");
 console.log(`New Array Planets - [${planets}]`);
 
 //Concatenacion
-let allArray = planets.concat(leaderboard); console.log(allArray);
+let allArray = planets.concat(leaderboard);
+console.log(allArray);
+
+//Reversa
+allArray.reverse();
+console.log(allArray);
+
+//Slice
+let slice = allArray.slice(1, 3);
+//con negativo por ejemplo -3 los ultimos 3.
+console.log(slice);
+
+//splice
+// array.splice(start[, deleteCount[, item1[, item2[, ...]]]])
+let nameList = ["Harry", "Lua", "Hermione", "Bellatrix"];
+nameList.splice(1, 0, "Agustin");
+console.log(nameList);
