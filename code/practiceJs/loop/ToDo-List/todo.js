716d68a54fc87a11f2ec6fa2552018a91c6da1a8
AgusBaez <agustinbaezignacio@gmail.com>
2022-06-21 16:20:01 -0300

    ToDo_List

--- code/practiceJs/loop/ToDo-List/todo.js
+++ code/practiceJs/loop/ToDo-List/todo.js
@@ -1,7 +1,22 @@
-let list = ['hello'];
-let popUp = prompt("Enter a Command".toLowerCase());
-
-// while (popUp === "new") {
-//   
-//   }
-// }
+const list = ["Hello", "GoodBye"];
+let popUp = prompt("Enter a Command").toLowerCase();
+while (popUp !== "quit") {
+  if (popUp === "new") {
+    let newTodo = prompt("Enter new Task To-Do List");
+    list.push(newTodo);
+    console.log(`${newTodo} add to list ${list}`);
+  } else if (popUp === "list") {
+    console.log("================");
+    for (let listador of list) {
+      console.log(listador);
+    }
+    console.log("================");
+  } else if (popUp === "delete") {
+    const focus = parseInt(prompt("Enter the Number of index task to Delet"));
+    console.log(list);
+    const deleted = list.splice(focus, 1);
+    console.log(`You deleted this ${deleted[0]}`);
+  }
+  popUp = prompt("Enter a Command - onLoop").toLowerCase();
+}
+console.log("GoodBye");
