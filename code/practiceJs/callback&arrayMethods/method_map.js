a2766b025ac9268c97b878d4e2398cc37217ddb6
AgusBaez <agustinbaezignacio@gmail.com>
2022-06-25 13:36:20 -0300

    exerciseMapMethod

---
+++ code/practiceJs/callback&arrayMethods/method_map.js
@@ -0,0 +1,13 @@
+const fullNames = [
+  { first: "Albus", last: "Dumbledore" },
+  { first: "Harry", last: "Potter" },
+  { first: "Hermione", last: "Granger" },
+  { first: "Ron", last: "Weasley" },
+  { first: "Rubeus", last: "Hagrid" },
+  { first: "Minerva", last: "McGonagall" },
+  { first: "Severus", last: "Snape" },
+];
+
+const firstNames = fullNames.map(function (names) {
+  console.log(`${names.first}`);
+});
